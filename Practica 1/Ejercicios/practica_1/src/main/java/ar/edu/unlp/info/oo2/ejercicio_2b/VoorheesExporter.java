2a85fed6ea13b3578d2a4fe2b18d4da4d8021c9b
Joaquin Spinelli <joakospinelli@gmail.com>
2022-03-23 17:45:00 -0300

    Ejercicio 2b terminado. Implementados test de Ej2 y Ej2b. UML de Ej2b subido

--- Practica 1/Ejercicios/practica_1/src/main/java/ar/edu/unlp/info/oo2/ejercicio_2b/VoorheesExporter.java
+++ Practica 1/Ejercicios/practica_1/src/main/java/ar/edu/unlp/info/oo2/ejercicio_2b/VoorheesExporter.java
@@ -3,8 +3,13 @@ package ar.edu.unlp.info.oo2.ejercicio_2b;
 import java.util.List;
 import org.json.simple.*;
 
+/* No sé si debería cambiar los métodos de esta clase para que
+ * trabajen con JSON o si debería hacer una clase nueva (PREGUNTAR)
+ */
+
 public class VoorheesExporter {
 
+	@SuppressWarnings("unchecked")
 	private static JSONObject crearJson(Socio s) {
 		JSONObject objectJson = new JSONObject();	
 		objectJson.put("name",s.getNombre());
@@ -14,11 +19,7 @@ public class VoorheesExporter {
 		return objectJson;
 	}
 	
-	private String exportar(Socio socio) {
-		
-		return crearJson(socio).toJSONString();
-	}
-	
+	@SuppressWarnings("unchecked")
 	public String exportar(List<Socio> socios) {
 		
 		JSONArray arrayJson = new JSONArray();
