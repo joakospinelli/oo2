b44ce303bfe903dc616ca869f71d3b421d33ac8d
Joaquin Spinelli <joakospinelli@gmail.com>
2022-03-29 23:23:03 -0300

    UML Ejercicios 1 y 2 (Práctica 2) + arreglo de errores

--- Practica/ejercicios/src/main/java/ar/edu/unlp/info/oo2/practica_2/ejercicio_1/Topografia.java
+++ Practica/ejercicios/src/main/java/ar/edu/unlp/info/oo2/practica_2/ejercicio_1/Topografia.java
@@ -2,7 +2,7 @@ package ar.edu.unlp.info.oo2.practica_2.ejercicio_1;
 
 public interface Topografia {
 	
-	public abstract double getProporcion();
+	public double getProporcion();
 	
 	public boolean esIgual(Topografia t);
 }
